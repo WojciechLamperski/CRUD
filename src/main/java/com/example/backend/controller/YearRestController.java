ee409bf936068bd304640dad8b895aa836365525
WojciechLamperski <wolamp20@gmail.com>
2025-01-27 18:37:42 +0100

    cleanup

--- src/main/java/com/example/backend/controller/YearRestController.java
+++ src/main/java/com/example/backend/controller/YearRestController.java
@@ -4,7 +4,6 @@ import com.example.backend.POJO.Year;
 import com.example.backend.service.YearService;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
